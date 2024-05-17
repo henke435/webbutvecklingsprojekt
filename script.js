ee58273ad0d4fcef5925b636c8b24a7ea4799dbe
henke435 <henrik.lindberg@skola.taby.se>
2024-05-17 23:55:16 +0200

    klart typ

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-document.getElementById('bankkortnummer').addEventListener('input', function (e) {
-    var input = e.target.value.replace(/\D/g, '').substring(0,16);
+document
+  .getElementById("bankkortnummer")
+  .addEventListener("input", function (e) {
+    var input = e.target.value.replace(/\D/g, "").substring(0, 16);
     var split = input.match(/.{1,4}/g);
     if (split) {
-        e.target.value = split.join('-');
+      e.target.value = split.join("-");
     } else {
-        e.target.value = '';
+      e.target.value = "";
     }
-});
-document.getElementById('utgangsdatum').addEventListener('input', function (e) {
-    var input = e.target.value.replace(/\D/g, '').substring(0,4);
-    var split = input.match(/.{1,2}/g);
-    if (split) {
-        e.target.value = split.join('/');
-    } else {
-        e.target.value = '';
-    }
+  });
+document.getElementById("utgangsdatum").addEventListener("input", function (e) {
+  var input = e.target.value.replace(/\D/g, "").substring(0, 4);
+  var split = input.match(/.{1,2}/g);
+  if (split) {
+    e.target.value = split.join("/");
+  } else {
+    e.target.value = "";
+  }
 });
